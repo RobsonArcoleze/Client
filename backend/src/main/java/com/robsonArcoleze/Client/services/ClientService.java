3fbaa325c4d22aa4044ae5c7893532db1dde68fd
RobsonDev <robson.arcoleze29@gmail.com>
2022-12-29 12:48:39 -0300

    Busca por ID

--- backend/src/main/java/com/robsonArcoleze/Client/services/ClientService.java
+++ backend/src/main/java/com/robsonArcoleze/Client/services/ClientService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.robsonArcoleze.Client.dto.ClientDTO;
 import com.robsonArcoleze.Client.entities.Client;
 import com.robsonArcoleze.Client.repositories.ClientRepository;
+import com.robsonArcoleze.Client.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -17,8 +18,15 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public Page<ClientDTO> findAllPaged(PageRequest request){
-		Page<Client> list = repository.findAll(request);
+	public Page<ClientDTO> findAllPaged(PageRequest pageable){
+		Page<Client> list = repository.findAll(pageable);
 		return list.map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Client client = repository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new ClientDTO(client);
 	}
 }
